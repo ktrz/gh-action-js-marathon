fd93cb5dd88b56161cbf57c47803411892d7bbb7
Chris Trzesniewski <k.trzesniewski@gmail.com>
2022-03-15 16:47:32 +0100

    print diff

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,43 @@
 import { getInput } from "@actions/core";
-import { context } from "@actions/github";
+import { context, getOctokit } from "@actions/github";
+import dedent from 'dedent'
 
-type GithubContext = typeof context
+type GithubContext = typeof context;
 
 const inputName = getInput("name");
+const ghToken = getInput("ghToken");
 
 greet(inputName, getRepoUrl(context));
 
-
-console.log('--- Action Info ---')
-console.log('eventName:', context.eventName)
-console.log('sha:', context.sha)
-console.log('ref:', context.ref)
-console.log('workflow:', context.workflow)
-console.log('action:', context.action)
-console.log('actor:', context.actor)
-console.log('job:', context.job)
-console.log('runNumber:', context.runNumber)
-console.log('runId:', context.runId)
-console.log('apiUrl:', context.apiUrl)
-console.log('serverUrl:', context.serverUrl)
-console.log('graphqlUrl:', context.graphqlUrl)
-console.log('-------------------')
-
-console.log('--- Payload ---')
-console.log(JSON.stringify(context.payload, undefined, 2))
-console.log('---------------')
+getDiff().then(files => {
+    console.log(dedent(`
+    Your PR diff:
+    ${JSON.stringify(files, undefined, 2)}
+    `))
+})
 
 function greet(name: string, repoUrl: string) {
   console.log(`'Hello ${name}! You are running a GH Action in ${repoUrl}'`);
 }
 
-function getRepoUrl({repo, serverUrl}: GithubContext): string {
-    return `${serverUrl}/${repo.owner}/${repo.repo}`
+function getRepoUrl({ repo, serverUrl }: GithubContext): string {
+  return `${serverUrl}/${repo.owner}/${repo.repo}`;
+}
+
+async function getDiff() {
+  if (ghToken && context.payload.pull_request) {
+      const octokit = getOctokit(ghToken)
+
+      const result = await octokit.rest.repos.compareCommits({
+          repo: context.repo.repo,
+          owner: context.repo.owner,
+          head: context.payload.pull_request.head.sha,
+          base: context.payload.pull_request.base.sha,
+          per_page: 100
+      })
+
+      return result.data.files || []
+  }
+
+  return []
 }
