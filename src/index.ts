88e376d4f302498ea55dbab564e12e7944c05997
Chris Trzesniewski <k.trzesniewski@gmail.com>
2022-03-15 16:24:40 +0100

    define and use input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+import { getInput } from "@actions/core"
+
+const inputName = getInput("name")
+
+greet(inputName)
+
+function greet(name: string) {
+    console.log(`'Hello ${name}!'`)
+}
+
+
